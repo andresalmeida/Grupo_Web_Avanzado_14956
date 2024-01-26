ebabb1aeaf2fab6b4903032087c3ab66cf1a038b
Andres Almeida <asalmeida4@espe.edu.ec>
2024-01-25 23:50:08 -0500

    Final Upload

---
+++ EvaluacionIntermedia V3.0/frontend/src/components/Greetings.jsx
@@ -0,0 +1,12 @@
+// frontend/src/components/Greetings.jsx
+import React from 'react';
+
+const Greetings = () => {
+    return (
+        <div>
+            <h1>Bienvenido al Sistema de Evaluaciones</h1>
+        </div>
+    );
+};
+
+export default Greetings;

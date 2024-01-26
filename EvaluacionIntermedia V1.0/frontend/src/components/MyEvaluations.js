b7be02ba5c95dcac45733495e6c2b1d664305f1d
Andres Almeida <asalmeida4@espe.edu.ec>
2024-01-25 20:59:33 -0500

    up

---
+++ EvaluacionIntermedia V1.0/frontend/src/components/MyEvaluations.js
@@ -0,0 +1,14 @@
+// frontend/src/components/MyEvaluations.js
+import React from 'react';
+
+const MyEvaluations = () => {
+    // Implement logic to display teacher's evaluations
+    return (
+        <div>
+            <h2>My Evaluations</h2>
+            {/* Display a list of teacher's evaluations */}
+        </div>
+    );
+};
+
+export default MyEvaluations;

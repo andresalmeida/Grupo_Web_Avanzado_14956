df1f6b0fc346ea76c33b0ddc33fd74e75cc3e391
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-01-25 23:22:49 -0500

    update

---
+++ EvaluacionIntermedia V2.0/database/evaluations.js
@@ -0,0 +1,8 @@
+// database/evaluations.js
+module.exports = [
+    { id: 1, title: 'Sample Evaluation', questions: [] },
+    // Add more evaluations as needed
+];
+
+// backend/database/evaluations.js
+module.exports = [];

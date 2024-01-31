c8c892423059f1e17b93622f1c5ef0dabe5fd8f4
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-01-30 23:30:56 -0500

    Update

---
+++ Proyecto-master/README.md
@@ -0,0 +1,5 @@
+- Install dependencies: `npm install` or `yarn`
+
+- Start the server: `npm run dev` or `yarn dev`
+
+- Views are on: `localhost:3000`
---
+++ Proyecto-master/src/sections/account/account-profile-details.js
@@ -0,0 +1,179 @@
+import { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+
+const states = [
+  {
+    value: 'alabama',
+    label: 'Quito'
+  },
+  {
+    value: 'new-york',
+    label: 'Sangolqui'
+  },
+  {
+    value: 'san-francisco',
+    label: 'Mitad del mundo'
+  },
+  {
+    value: 'los-angeles',
+    label: 'Calacali'
+  }
+];
+
+export const AccountProfileDetails = () => {
+  const [values, setValues] = useState({
+    firstName: 'Paola',
+    lastName: 'D Link',
+    email: 'pao@gmail.com',
+    phone: 'para que sapitos',
+    state: 'Quito',
+    country: 'Ecuador'
+  });
+
+  const handleChange = useCallback(
+    (event) => {
+      setValues((prevState) => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+      }));
+    },
+    []
+  );
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+    },
+    []
+  );
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      onSubmit={handleSubmit}
+    >
+      <Card>
+        <CardHeader
+          subheader="La información puede editarse"
+          title="Perfil"
+        />
+        <CardContent sx={{ pt: 0 }}>
+          <Box sx={{ m: -1.5 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  helperText="Especifique el nombre"
+                  label="Nombre"
+                  name="firstName"
+                  onChange={handleChange}
+                  required
+                  value={values.firstName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Apellido"
+                  name="lastName"
+                  onChange={handleChange}
+                  required
+                  value={values.lastName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Correo"
+                  name="email"
+                  onChange={handleChange}
+                  required
+                  value={values.email}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Telefono"
+                  name="phone"
+                  onChange={handleChange}
+                  type="number"
+                  value={values.phone}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="País"
+                  name="country"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Ciudad"
+                  name="state"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.state}
+                >
+                  {states.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ justifyContent: 'flex-end' }}>
+          <Button variant="contained">
+            Guardar
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};

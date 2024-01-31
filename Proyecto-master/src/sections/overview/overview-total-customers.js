4c6101951ea49d1a6240ababb01d3d6b08910c03
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-01-31 10:14:53 -0500

    update

--- Proyecto-master/src/sections/overview/overview-total-customers.js
+++ Proyecto-master/src/sections/overview/overview-total-customers.js
@@ -21,7 +21,7 @@ export const OverviewTotalCustomers = (props) => {
               color="text.secondary"
               variant="overline"
             >
-              Total Customers
+              Total Proveedores
             </Typography>
             <Typography variant="h4">
               {value}
@@ -68,7 +68,7 @@ export const OverviewTotalCustomers = (props) => {
               color="text.secondary"
               variant="caption"
             >
-              Since last month
+              Mes pasado
             </Typography>
           </Stack>
         )}

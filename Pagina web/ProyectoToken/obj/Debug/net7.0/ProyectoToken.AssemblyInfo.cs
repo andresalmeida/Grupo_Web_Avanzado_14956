c7284d5a8462a740531e58af1f0842e00fcc5168
PaolaMoncayo <apmoncayo2@espe.edu.ec>
2024-02-04 19:02:37 -0500

    commit

--- Pagina web/ProyectoToken/obj/Debug/net7.0/ProyectoToken.AssemblyInfo.cs
+++ Pagina web/ProyectoToken/obj/Debug/net7.0/ProyectoToken.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoToken")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ffa9c63373bfc3e78576408f67be296d221f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5bdcc257faf8e03c51cf006205ce6faeb8fb9ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoToken")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoToken")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

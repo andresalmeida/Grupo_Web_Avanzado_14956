3e67c6f3be9bdf0e0a9687e999e46d574a9e607f
Darwin Roberto Valdiviezo Condor <drvaldiviezo2@espe.edu.ec>
2024-02-06 20:53:05 -0500

    update v2

---
+++ Proyecto-Master-V2/frontend/README.md
@@ -0,0 +1,15 @@
+# BACKEND
+
+### `npm i`
+
+### `npm run backend`
+
+# FRONTEND
+
+### `npm i`
+
+### `npm install --global windows-build-tools`
+
+### `npm install node-sass@latest`
+
+### `npm run start`
---
+++ Proyecto-Master-V2/frontend/src/data/sidebar.js
@@ -0,0 +1,36 @@
+import { FaTh, FaRegChartBar, FaCommentAlt } from "react-icons/fa";
+import { BiImageAdd } from "react-icons/bi";
+
+const menu = [
+  {
+    title: "Panel de Control",
+    icon: <FaTh />,
+    path: "/dashboard",
+  },
+  {
+    title: "Añadir productos",
+    icon: <BiImageAdd />,
+    path: "/add-product",
+  },
+  {
+    title: "Cuenta de usuario",
+    icon: <FaRegChartBar />,
+    childrens: [
+      {
+        title: "Perfil",
+        path: "/profile",
+      },
+      {
+        title: "Editar Perfil",
+        path: "/edit-profile",
+      },
+    ],
+  },
+  {
+    title: "Report Bug",
+    icon: <FaCommentAlt />,
+    path: "/contact-us",
+  },
+];
+
+export default menu;
